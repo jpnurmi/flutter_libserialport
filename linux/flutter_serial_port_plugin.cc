0bbbb6af50bce880d10fe6c5f15758e878f415a2
J-P Nurmi <jpnurmi@gmail.com>
2020-09-27 18:18:28 +0200

    Linux: define LIBSERIALPORT_PATH environment variable

--- linux/flutter_serial_port_plugin.cc
+++ linux/flutter_serial_port_plugin.cc
@@ -1,8 +1,7 @@
 #include "include/flutter_serial_port/flutter_serial_port_plugin.h"
 
 #include <flutter_linux/flutter_linux.h>
-#include <gtk/gtk.h>
-#include <sys/utsname.h>
+#include <glib.h>
 
 #define FLUTTER_SERIAL_PORT_PLUGIN(obj) \
   (G_TYPE_CHECK_INSTANCE_CAST((obj), flutter_serial_port_plugin_get_type(), \
@@ -14,55 +13,27 @@ struct _FlutterSerialPortPlugin {
 
 G_DEFINE_TYPE(FlutterSerialPortPlugin, flutter_serial_port_plugin, g_object_get_type())
 
-// Called when a method call is received from Flutter.
-static void flutter_serial_port_plugin_handle_method_call(
-    FlutterSerialPortPlugin* self,
-    FlMethodCall* method_call) {
-  g_autoptr(FlMethodResponse) response = nullptr;
-
-  const gchar* method = fl_method_call_get_name(method_call);
-
-  if (strcmp(method, "getPlatformVersion") == 0) {
-    struct utsname uname_data = {};
-    uname(&uname_data);
-    g_autofree gchar *version = g_strdup_printf("Linux %s", uname_data.version);
-    g_autoptr(FlValue) result = fl_value_new_string(version);
-    response = FL_METHOD_RESPONSE(fl_method_success_response_new(result));
-  } else {
-    response = FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
-  }
-
-  fl_method_call_respond(method_call, response, nullptr);
-}
-
-static void flutter_serial_port_plugin_dispose(GObject* object) {
-  G_OBJECT_CLASS(flutter_serial_port_plugin_parent_class)->dispose(object);
-}
-
-static void flutter_serial_port_plugin_class_init(FlutterSerialPortPluginClass* klass) {
-  G_OBJECT_CLASS(klass)->dispose = flutter_serial_port_plugin_dispose;
-}
+static void flutter_serial_port_plugin_class_init(FlutterSerialPortPluginClass* klass) {}
 
 static void flutter_serial_port_plugin_init(FlutterSerialPortPlugin* self) {}
 
-static void method_call_cb(FlMethodChannel* channel, FlMethodCall* method_call,
-                           gpointer user_data) {
-  FlutterSerialPortPlugin* plugin = FLUTTER_SERIAL_PORT_PLUGIN(user_data);
-  flutter_serial_port_plugin_handle_method_call(plugin, method_call);
+static gchar* g_self_exe() {
+  g_autoptr(GError) error = nullptr;
+  g_autofree gchar* self_exe = g_file_read_link("/proc/self/exe", &error);
+  if (error) {
+    g_critical("g_file_read_link: %s", error->message);
+  }
+  return g_path_get_dirname(self_exe);
 }
 
 void flutter_serial_port_plugin_register_with_registrar(FlPluginRegistrar* registrar) {
   FlutterSerialPortPlugin* plugin = FLUTTER_SERIAL_PORT_PLUGIN(
       g_object_new(flutter_serial_port_plugin_get_type(), nullptr));
 
-  g_autoptr(FlStandardMethodCodec) codec = fl_standard_method_codec_new();
-  g_autoptr(FlMethodChannel) channel =
-      fl_method_channel_new(fl_plugin_registrar_get_messenger(registrar),
-                            "flutter_serial_port",
-                            FL_METHOD_CODEC(codec));
-  fl_method_channel_set_method_call_handler(channel, method_call_cb,
-                                            g_object_ref(plugin),
-                                            g_object_unref);
+  g_autofree gchar* self_exe = g_self_exe();
+  g_autofree gchar* libserialport_path =
+      g_build_filename(self_exe, "lib", "libserialport.so", nullptr);
+  setenv("LIBSERIALPORT_PATH", libserialport_path, 0);
 
   g_object_unref(plugin);
 }
