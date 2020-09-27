3aa33e112377e40c2095900a877ef457f8937a72
J-P Nurmi <jpnurmi@gmail.com>
2020-09-27 16:54:15 +0200

    Export dart_serial_port

--- lib/flutter_serial_port.dart
+++ lib/flutter_serial_port.dart
@@ -1,14 +1,1 @@
-
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class FlutterSerialPort {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_serial_port');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'package:dart_serial_port/dart_serial_port.dart';
