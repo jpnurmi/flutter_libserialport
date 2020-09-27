a72da9547e9a606ef69b427c093b500797c7c7a1
J-P Nurmi <jpnurmi@gmail.com>
2020-09-27 16:55:36 +0200

    Add simple example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,45 +1,41 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_serial_port/flutter_serial_port.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(ExampleApp());
 
-class MyApp extends StatefulWidget {
+class ExampleApp extends StatefulWidget {
   @override
-  _MyAppState createState() => _MyAppState();
+  _ExampleAppState createState() => _ExampleAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+extension IntToString on int {
+  String toHex() => '0x${toRadixString(16)}';
+  String toPadded([int width = 3]) => toString().padLeft(width, '0');
+  String toTransport() {
+    switch (this) {
+      case SerialPortTransport.usb:
+        return 'USB';
+      case SerialPortTransport.bluetooth:
+        return 'Bluetooth';
+      case SerialPortTransport.native:
+        return 'Native';
+      default:
+        return 'Unknown';
+    }
+  }
+}
+
+class _ExampleAppState extends State<ExampleApp> {
+  var availablePorts = [];
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
+    initPorts();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterSerialPort.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+  void initPorts() {
+    setState(() => availablePorts = SerialPort.availablePorts);
   }
 
   @override
@@ -47,11 +43,54 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Flutter Serial Port example'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Scrollbar(
+          child: ListView(
+            children: [
+              for (final address in availablePorts)
+                Builder(builder: (context) {
+                  final port = SerialPort(address);
+                  return ExpansionTile(
+                    title: Text(address),
+                    children: [
+                      CardListTile('Description', port.description),
+                      CardListTile('Transport', port.transport.toTransport()),
+                      CardListTile('USB Bus', port.busNumber.toPadded()),
+                      CardListTile('USB Device', port.deviceNumber.toPadded()),
+                      CardListTile('Vendor ID', port.vendorId.toHex()),
+                      CardListTile('Product ID', port.productId.toHex()),
+                      CardListTile('Manufacturer', port.manufacturer),
+                      CardListTile('Product Name', port.productName),
+                      CardListTile('Serial Number', port.serialNumber),
+                      CardListTile('MAC Address', port.macAddress),
+                    ],
+                  );
+                }),
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          onPressed: initPorts,
+        ),
+      ),
+    );
+  }
+}
+
+class CardListTile extends StatelessWidget {
+  final String name;
+  final String value;
+
+  CardListTile(this.name, this.value);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(value ?? 'N/A'),
+        subtitle: Text(name),
       ),
     );
   }
